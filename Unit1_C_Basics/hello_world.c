d9e16b1f1d3bb12b570592b293720d52cc9c1307
Ryan Aparicio <aparicioryan2003@gmail.com>
2024-01-25 10:18:10 -0600

    tryign to fix push

--- Unit1_C_Basics/hello_world.c
+++ Unit1_C_Basics/hello_world.c
@@ -1,4 +1,5 @@
 //// Ryan Aparicio
+////heasd;fasd;kljf 
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,7 +12,18 @@ int main(int argc, char* argv[]){
 
     printf("%d) option %s", 1, "one");
 
+    
+
+    for (int x = 0; x< NUMBER; x++){
+        //do something
+    }
+    int whole_number = 6;
+    int* whole_number_pointer = &whole_number;
+
+    *whole_number_pointer = 46;//* is dereferencing the value
+    printf("\n%d", whole_number);
+    printf("\n%d", *whole_number_pointer);
+    printf("\n%p", whole_number_pointer)
+
     return 0; // no errors
-
-
 }
