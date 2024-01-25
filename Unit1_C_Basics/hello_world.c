0673c98bc06c1c721911b47082dff7c8e4681a26
Ryan Aparicio <aparicioryan2003@gmail.com>
2024-01-25 09:46:14 -0600

    c praictice

---
+++ Unit1_C_Basics/hello_world.c
@@ -0,0 +1,17 @@
+//// Ryan Aparicio
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char* argv[]){
+    printf("Hello World");
+    printf("🥸");
+
+    printf("%d) option %s", 1, "one");
+
+    return 0; // no errors
+
+
+}
