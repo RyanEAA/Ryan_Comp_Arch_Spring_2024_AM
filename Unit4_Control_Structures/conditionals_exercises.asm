c3539c1472367cde061bd91c889461777b692297
root <aparicioryan2003@gmail.com>
2024-02-22 22:19:26 +0000

    edited file to practice is else

--- Unit4_Control_Structures/conditionals_exercises.asm
+++ Unit4_Control_Structures/conditionals_exercises.asm
@@ -7,7 +7,12 @@
 
 
 segment .data					; initialized data
+        if_prompt db "enter a character: ", 0
+        if_output db "and ... your point is", 0
 
+        ifelse_prompt db "enter a number: ",0
+        even db "EVEN", 0
+        odd db "ODD", 0
 
 segment .bss					; uninitialized data
 
@@ -20,12 +25,42 @@ asm_main:
         pusha
 
 	; TODO: IF EXERCISE
+        ; mov eax, if_prompt
+        ; call print_string
+        ; call read_char
 
+        ; mov eax, if_prompt
+        ; call print_string
+        ; call read_char
+
+        ; cmp eax, "&"
+        ; jne endif ; if not an & jump to endif
+        ; mov eax, if_ouput
+        ; call print_string
+
+        ; call read_char ; this consumes the new line character
 
 	; TODO: IF/ELSE EXERCISE
+        mov eax, ifelse_prompt
+        call print_string
+        call read_int
+
+        and eax, 1 ; get last bit by itself
+        cmp eax, 0
+        je even_block
+
+        mov eax, odd
+        jmp end_ifelse
+
+even_block:
+        mov eax, even
+
+end_ifelse:
+        call print_string
 
 
 	; TODO: ELIF EXERCISE
+
 
 
 	; TODO: AND EXERCISE
